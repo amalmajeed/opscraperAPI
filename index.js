d1eb18b754860051549418ced0b4514edd3b58b3
Amal Majeed <amalmajeed7@gmail.com>
2022-06-29 22:26:40 -0600

    Episode - title API done

--- index.js
+++ index.js
@@ -19,16 +19,10 @@ let pwd = data[1];
 let mongoURL = `mongodb+srv://${uname}:${pwd}@episodestochapters.l8qrtlg.mongodb.net/?retryWrites=true&w=majority`;
 const dbClient = new MongoClient(mongoURL);
 
-// Routing logic
-app.route("/episodeName/:ep").get((request, response, next) => {
-  response.send(`This is hello from episodeName ${request.params.ep}`);
-  //   IF ELSE LOGIC TO CHECK IF VALID EPISODE !
-});
+// ALL ROUTING LOGIC
 
-app.listen(PORT, async () => {
-  console.log(
-    `=============================The One Piece Scraper API service is running live on port: ${PORT} ============================= \n`
-  );
+// episodetottileAPI
+app.route("/episodetitle/:ep").get(async (request, response, next) => {
   //   DB CONNECTION LOGIC HERE !
   try {
     await dbClient.connect();
@@ -37,13 +31,58 @@ app.listen(PORT, async () => {
     console.log(`Connected to DB 'animetomanga' successfully !`);
     const e2title = await opdb.collection("episodetotitle");
     const cursr = await e2title.find({ _id: "onepiecetitles" });
-    const map = await cursr.toArray();
-    console.log(map);
-    console.log(`Connected to collection 'episodetotitle' successfully !`);
+    // The below variable is used outside the try block - hence var function scope is necessary
+    var map = await cursr.toArray();
+    console.log(
+      `Succesfully retrieved the episode to title maps from collection 'episodetotitle' !`
+    );
     dbClient.close().then(() => {
       console.log("Connection closed successfully !");
     });
   } catch (err) {
     console.error(err);
+    server.close(() => {
+      console.log(`Connection to the server at port ${PORT} has been closed !`);
+    });
   }
+  let epMap = map[0].content;
+  try {
+    let mapKeys = Object.keys(epMap);
+    if (mapKeys.includes(request.params.ep)) {
+      response.json({
+        episode: request.params.ep,
+        title: epMap[request.params.ep],
+      });
+    } else {
+      response.json({ error: `No entry for episode ${request.params.ep}` });
+    }
+  } catch (err) {
+    response.json({ error: err });
+  }
+});
+
+var server = app.listen(PORT, async () => {
+  console.log(
+    `=============================The One Piece Scraper API service is running live on port: ${PORT} ============================= \n`
+  );
+  //   //   DB CONNECTION LOGIC HERE !
+  //   try {
+  //     await dbClient.connect();
+  //     console.log(`Connected to cluster 'episodestochapters' successfully !`);
+  //     const opdb = await dbClient.db("animetomanga");
+  //     console.log(`Connected to DB 'animetomanga' successfully !`);
+  //     const e2title = await opdb.collection("episodetotitle");
+  //     const cursr = await e2title.find({ _id: "onepiecetitles" });
+  //     const map = await cursr.toArray();
+  //     console.log(`Succesfully retrieved the episode to title maps`);
+  //     console.log(`Connected to collection 'episodetotitle' successfully !`);
+  //     dbClient.close().then(() => {
+  //       console.log("Connection closed successfully !");
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //     server.close(() => {
+  //       console.log(`Connection to the server at port ${PORT} has been closed !`);
+  //     });
+  //   }
 });
