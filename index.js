51de4a6773e96cb40ab6273b56ffd7c48943b5c0
Amal Majeed <amalmajeed7@gmail.com>
2022-07-01 09:00:39 -0600

    http server converted to TLS server

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import fs from "fs";
+import https from "https";
 import { MongoClient } from "mongodb";
 
 const app = express();
 const PORT = 5000;
+
+// app.use(express.json());
+
+var jsonParser = express.json();
+var urlencodedParser = express.urlencoded({ extended: false });
 
 // Obtaining the username and password for DB access
 const data = fs
@@ -37,7 +43,7 @@ app.route("/episodetitle/:ep").get(async (request, response, next) => {
       `Succesfully retrieved the episode to title maps from collection 'episodetotitle' !`
     );
     dbClient.close().then(() => {
-      console.log("Connection closed successfully !");
+      console.log("Connection to DB closed successfully !");
     });
   } catch (err) {
     console.error(err);
@@ -61,9 +67,81 @@ app.route("/episodetitle/:ep").get(async (request, response, next) => {
   }
 });
 
-// APP / SERVER
+// episodetomangachaptersAPI
+app.route("/episodechapters/:ep").get(async (request, response, next) => {
+  //   DB CONNECTION LOGIC HERE !
+  try {
+    await dbClient.connect();
+    console.log(`Connected to cluster 'episodestochapters' successfully !`);
+    const opdb = await dbClient.db("animetomanga");
+    console.log(`Connected to DB 'animetomanga' successfully !`);
+    const e2chap = await opdb.collection("episodetochapter");
+    const cursr = await e2chap.find({ _id: "onepiecemap" });
+    // The below variable is used outside the try block - hence var function scope is necessary
+    var map = await cursr.toArray();
+    console.log(
+      `Succesfully retrieved the episode to manga chapter list map from collection 'episodetochapter' !`
+    );
+    dbClient.close().then(() => {
+      console.log("Connection to DB closed successfully !");
+    });
+  } catch (err) {
+    console.error(err);
+    sslServer.close(() => {
+      console.log(`Connection to the server at port ${PORT} has been closed !`);
+    });
+  }
+  let epMap = map[0].content;
+  try {
+    let mapKeys = Object.keys(epMap);
+    if (mapKeys.includes(request.params.ep)) {
+      response.json({
+        episode: request.params.ep,
+        chapters: epMap[request.params.ep],
+      });
+    } else {
+      response.json({ error: `No entry for episode ${request.params.ep}` });
+    }
+  } catch (err) {
+    response.json({ error: err });
+  }
+});
 
-var server = app.listen(PORT, async () => {
+app.route("/api").post(jsonParser, (request, response) => {
+  try {
+    let serviceName = request.body.serviceName;
+    let episodeNumber = request.body.episodeNumber;
+    if (!serviceName) {
+      throw new Error("Missing parameter 'serviceName'");
+    }
+    if (!episodeNumber) {
+      throw new Error("Missing parameter 'episodeNumber'");
+    }
+    response.redirect(301, `/${serviceName}/${episodeNumber}`);
+  } catch (err) {
+    response.status = 400;
+    response.json({
+      error: `400 : Bad request => ${err}`,
+    });
+  }
+});
+
+app.route("/a").get((req, res) => {
+  console.log("Hello!");
+  res.send("Hello!");
+});
+
+// APP  run on an SSL SERVER
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync("./cert/key.pem"),
+    cert: fs.readFileSync("./cert/cert.pem"),
+  },
+  app
+);
+
+sslServer.listen(PORT, async () => {
   console.log(
     `=============================The One Piece Scraper API service is running live on port: ${PORT} ============================= \n`
   );
